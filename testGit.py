0764e1ee626fb4f0c6cb4fb2259e66444d4f2fa6
Nancy <nancysackman@D-10-19-226-108.dhcp4.washington.edu>
2018-06-01 15:05:22 -0700

    testSecond

--- testGit.py
+++ testGit.py
@@ -8,3 +8,11 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 print("hello world")
+
+ef find_force(mass, acceleration):
+    #do something here so don't need to define variable, evaluates rightside before left side
+    return mass*acceleration
+
+
+force=find_force(10, 9.8)
+print(force)
